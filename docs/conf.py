faeec6ee79a41cf70c094469ed0548744c4358b2
Dan Ryan <dan@danryan.co>
2018-05-10 23:05:39 -0400

    Update documentation config
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- docs/conf.py
+++ docs/conf.py
@@ -92,26 +92,23 @@ exclude_patterns = ['_build', '_man', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 
 extlinks = {
     'issue': ('https://github.com/techalchemy/requirementslib/issues/%s', '#'),
     'pull': ('https://github.com/techalchemy/requirementslib/pull/%s', 'PR #'),
 }
 html_theme_options = {
-    # 'collapsiblesidebar': True,
-    # 'externalrefs': True,
-    # 'issues_url': 'https://github.com/techalchemy/requirementslib/issues',
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
-    # 'style_external_links': False,
-    # 'vcs_pageview_mode': '',
+    'style_external_links': True,
+    'vcs_pageview_mode': '',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
-    # 'titles_only': False
+    'titles_only': False
 }
 
 html_last_updated_fmt = '%b %d, %Y'
@@ -129,7 +126,6 @@ html_sidebars = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
