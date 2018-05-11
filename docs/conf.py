161438f971f79a6dafe8f1247d8882b5202043a1
Dan Ryan <dan@danryan.co>
2018-05-10 22:52:25 -0400

    Update docs config
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,7 @@ html_theme_options = {
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
-    'titles_only': False
+    # 'titles_only': False
 }
 
 html_last_updated_fmt = '%b %d, %Y'
