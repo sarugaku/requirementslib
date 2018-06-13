d9b39ff0e9d866df2b012a647bfd21aa49370ff2
Tzu-ping Chung <uranusjr@gmail.com>
2018-06-13 19:42:20 +0800

    Rewrite get_link_from_line and add notes
    Hopefully this makes sense. I am very not sure if I get all the nuances
    of each variants, so please please review this.

--- src/requirementslib/models/utils.py
+++ src/requirementslib/models/utils.py
@@ -88,7 +88,7 @@ def add_ssh_scheme_to_git_uri(uri):
     if isinstance(uri, six.string_types):
         # Add scheme for parsing purposes, this is also what pip does
         if uri.startswith("git+") and "://" not in uri:
-            uri = uri.replace("git+", "git+ssh://")
+            uri = uri.replace("git+", "git+ssh://", 1)
     return uri
 
 
