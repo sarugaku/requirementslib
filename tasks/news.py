660914787840f56a4ba41e25bb24c290cccc8c08
Dan Ryan <dan@danryan.co>
2018-06-15 22:19:03 -0400

    Fix news gen tasks
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- tasks/news.py
+++ tasks/news.py
@@ -35,11 +35,11 @@ def add(ctx, description, type_='feature', issue=None):
     target_dir = _get_news_dir(ctx)
     existing_files = [f.suffix.split('-')[0] for f in target_dir.glob('{0}.*'.format(type_)) if f.suffix.split('-')[0].isdigit()]
     if not existing_files and not issue:
-        nextfile = target_dir / '1-{0}.{1}'.format(type_, get_random())
+        nextfile = target_dir / '1-{0}.{1}'.format(get_random(), type_)
     elif issue:
         nextfile = target_dir / '{0}.{1}'.format(issue, type_)
     else:
-        nextfile = '{0}-{1}.{2}'.format(type_, int(sorted(existing_files).pop()) + 1, get_random())
+        nextfile = '{0}-{1}.{2}'.format(int(sorted(existing_files).pop()) + 1, get_random(), type_)
         nextfile = target_dir / nextfile
     print("[news.add] Adding newsfile {0} => [{1}] {2}".format(nextfile.name, type_.upper(), description))
     nextfile.write_text(description)
