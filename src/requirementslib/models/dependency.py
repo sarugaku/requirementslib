484045f81e225e1ff519475d6b8877d59bd6c106
Dan Ryan <dan@danryan.co>
2018-07-24 01:11:47 -0400

    Add resolver notes
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/requirementslib/models/dependency.py
+++ src/requirementslib/models/dependency.py
@@ -1,4 +1,7 @@
 # -*- coding=utf-8 -*-
+import copy
+from first import first
+from .utils import full_groupby, key_from_ireq
 from .._compat import RequirementPreparer, Resolver, WheelCache, RequirementSet, PackageFinder, TemporaryDirectory
 from ..utils import fs_str, prepare_pip_source_args, get_pip_command, prepare_pip_source_args
 import os
@@ -55,6 +58,16 @@ def get_resolver(sources=None):
     return finder, preparer, resolver
 
 
+def find_all_matches(finder, ireq):
+    candidates = finder.find_all_candidates(ireq.name)
+    matches = [m for m in candidates if m.version in ireq.specifier]
+    return matches
+
+
+def get_match_dependencies(ireqs):
+    return [get_dependencies(ir) for ir in ireqs]
+
+
 def get_dependencies(dep, sources=None):
     dep.is_direct = True
     reqset = RequirementSet()
@@ -64,3 +77,37 @@ def get_dependencies(dep, sources=None):
     requirements = reqset.requirements.values()
     reqset.cleanup_files()
     return requirements
+
+
+def get_grouped_dependencies(constraints):
+    # We need to track what contributed a specifierset
+    # as well as which specifiers were required by the root node
+    # in order to resolve any conflicts when we are deciding which thing to backtrack on
+    # then we take the loose match (which _is_ flexible) and start moving backwards in
+    # versions by popping them off of a stack and checking for the conflicting package
+    for _, ireqs in full_groupby(constraints, key=key_from_ireq):
+        ireqs = list(ireqs)
+        editable_ireq = first(ireqs, key=lambda ireq: ireq.editable)
+        if editable_ireq:
+            yield editable_ireq  # ignore all the other specs: the editable one is the one that counts
+            continue
+        ireqs = iter(ireqs)
+        # deepcopy the accumulator so as to not modify the self.our_constraints invariant
+        combined_ireq = copy.deepcopy(next(ireqs))
+        for ireq in ireqs:
+            # NOTE we may be losing some info on dropped reqs here
+            try:
+                combined_ireq.req.specifier &= ireq.req.specifier
+            except TypeError:
+                if ireq.req.specifier._specs and not combined_ireq.req.specifier._specs:
+                    combined_ireq.req.specifier._specs = ireq.req.specifier._specs
+            combined_ireq.constraint &= ireq.constraint
+            if not combined_ireq.markers:
+                combined_ireq.markers = ireq.markers
+            else:
+                _markers = combined_ireq.markers._markers
+                if not isinstance(_markers[0], (tuple, list)):
+                    combined_ireq.markers._markers = [_markers, 'and', ireq.markers._markers]
+            # Return a sorted, de-duped tuple of extras
+            combined_ireq.extras = tuple(sorted(set(tuple(combined_ireq.extras) + tuple(ireq.extras))))
+        yield combined_ireq
--- src/requirementslib/models/utils.py
+++ src/requirementslib/models/utils.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 import os
 import six
 from attr import validators
+from collections import OrderedDict
+from itertools import chain, groupby
 from first import first
 from .._compat import Link
 from ..utils import SCHEME_LIST, VCS_LIST, is_star
@@ -149,3 +151,211 @@ def validate_specifiers(instance, attr_, value):
         SpecifierSet(value)
     except (InvalidMarker, InvalidSpecifier):
         raise ValueError("Invalid Specifiers {0}".format(value))
+
+
+def key_from_ireq(ireq):
+    """Get a standardized key for an InstallRequirement."""
+    if ireq.req is None and ireq.link is not None:
+        return str(ireq.link)
+    else:
+        return key_from_req(ireq.req)
+
+
+def key_from_req(req):
+    """Get an all-lowercase version of the requirement's name."""
+    if hasattr(req, 'key'):
+        # from pkg_resources, such as installed dists for pip-sync
+        key = req.key
+    else:
+        # from packaging, such as install requirements from requirements.txt
+        key = req.name
+
+    key = key.replace('_', '-').lower()
+    return key
+
+
+def _requirement_to_str_lowercase_name(requirement):
+    """
+    Formats a packaging.requirements.Requirement with a lowercase name.
+
+    This is simply a copy of
+    https://github.com/pypa/packaging/blob/16.8/packaging/requirements.py#L109-L124
+    modified to lowercase the dependency name.
+
+    Previously, we were invoking the original Requirement.__str__ method and
+    lowercasing the entire result, which would lowercase the name, *and* other,
+    important stuff that should not be lowercased (such as the marker). See
+    this issue for more information: https://github.com/pypa/pipenv/issues/2113.
+    """
+    parts = [requirement.name.lower()]
+
+    if requirement.extras:
+        parts.append("[{0}]".format(",".join(sorted(requirement.extras))))
+
+    if requirement.specifier:
+        parts.append(str(requirement.specifier))
+
+    if requirement.url:
+        parts.append("@ {0}".format(requirement.url))
+
+    if requirement.marker:
+        parts.append("; {0}".format(requirement.marker))
+
+    return "".join(parts)
+
+
+def format_requirement(ireq, marker=None):
+    """
+    Generic formatter for pretty printing InstallRequirements to the terminal
+    in a less verbose way than using its `__str__` method.
+    """
+    if ireq.editable:
+        line = '-e {}'.format(ireq.link)
+    else:
+        line = _requirement_to_str_lowercase_name(ireq.req)
+
+    if marker and ';' not in line:
+        line = '{}; {}'.format(line, marker)
+
+    return line
+
+
+def format_specifier(ireq):
+    """
+    Generic formatter for pretty printing the specifier part of
+    InstallRequirements to the terminal.
+    """
+    # TODO: Ideally, this is carried over to the pip library itself
+    specs = ireq.specifier._specs if ireq.req is not None else []
+    specs = sorted(specs, key=lambda x: x._spec[1])
+    return ','.join(str(s) for s in specs) or '<any>'
+
+
+def is_pinned_requirement(ireq):
+    """
+    Returns whether an InstallRequirement is a "pinned" requirement.
+
+    An InstallRequirement is considered pinned if:
+
+    - Is not editable
+    - It has exactly one specifier
+    - That specifier is "=="
+    - The version does not contain a wildcard
+
+    Examples:
+        django==1.8   # pinned
+        django>1.8    # NOT pinned
+        django~=1.8   # NOT pinned
+        django==1.*   # NOT pinned
+    """
+    if ireq.editable:
+        return False
+
+    if len(ireq.specifier._specs) != 1:
+        return False
+
+    op, version = first(ireq.specifier._specs)._spec
+    return (op == '==' or op == '===') and not version.endswith('.*')
+
+
+def as_tuple(ireq):
+    """
+    Pulls out the (name: str, version:str, extras:(str)) tuple from the pinned InstallRequirement.
+    """
+    if not is_pinned_requirement(ireq):
+        raise TypeError('Expected a pinned InstallRequirement, got {}'.format(ireq))
+
+    name = key_from_req(ireq.req)
+    version = first(ireq.specifier._specs)._spec[1]
+    extras = tuple(sorted(ireq.extras))
+    return name, version, extras
+
+
+def full_groupby(iterable, key=None):
+    """Like groupby(), but sorts the input on the group key first."""
+    return groupby(sorted(iterable, key=key), key=key)
+
+
+def flat_map(fn, collection):
+    """Map a function over a collection and flatten the result by one-level"""
+    return chain.from_iterable(map(fn, collection))
+
+
+def lookup_table(values, key=None, keyval=None, unique=False, use_lists=False):
+    """
+    Builds a dict-based lookup table (index) elegantly.
+
+    Supports building normal and unique lookup tables.  For example:
+
+    >>> assert lookup_table(
+    ...     ['foo', 'bar', 'baz', 'qux', 'quux'], lambda s: s[0]) == {
+    ...     'b': {'bar', 'baz'},
+    ...     'f': {'foo'},
+    ...     'q': {'quux', 'qux'}
+    ... }
+
+    For key functions that uniquely identify values, set unique=True:
+
+    >>> assert lookup_table(
+    ...     ['foo', 'bar', 'baz', 'qux', 'quux'], lambda s: s[0],
+    ...     unique=True) == {
+    ...     'b': 'baz',
+    ...     'f': 'foo',
+    ...     'q': 'quux'
+    ... }
+
+    The values of the resulting lookup table will be values, not sets.
+
+    For extra power, you can even change the values while building up the LUT.
+    To do so, use the `keyval` function instead of the `key` arg:
+
+    >>> assert lookup_table(
+    ...     ['foo', 'bar', 'baz', 'qux', 'quux'],
+    ...     keyval=lambda s: (s[0], s[1:])) == {
+    ...     'b': {'ar', 'az'},
+    ...     'f': {'oo'},
+    ...     'q': {'uux', 'ux'}
+    ... }
+
+    """
+    if keyval is None:
+        if key is None:
+            keyval = (lambda v: v)
+        else:
+            keyval = (lambda v: (key(v), v))
+
+    if unique:
+        return dict(keyval(v) for v in values)
+
+    lut = {}
+    for value in values:
+        k, v = keyval(value)
+        try:
+            s = lut[k]
+        except KeyError:
+            if use_lists:
+                s = lut[k] = list()
+            else:
+                s = lut[k] = set()
+        if use_lists:
+            s.append(v)
+        else:
+            s.add(v)
+    return dict(lut)
+
+
+def dedup(iterable):
+    """Deduplicate an iterable object like iter(set(iterable)) but
+    order-reserved.
+    """
+    return iter(OrderedDict.fromkeys(iterable))
+
+
+def name_from_req(req):
+    """Get the name of the requirement"""
+    if hasattr(req, 'project_name'):
+        # from pkg_resources, such as installed dists for pip-sync
+        return req.project_name
+    else:
+        # from packaging, such as install requirements from requirements.txt
+        return req.name
