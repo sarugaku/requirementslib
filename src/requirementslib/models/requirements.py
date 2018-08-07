ab5877d7502468c87cdc09827d766e8b3b7a81d6
Tzu-ping Chung <uranusjr@gmail.com>
2018-08-08 03:20:30 +0800

    Allow formatting line without hash

--- src/requirementslib/models/requirements.py
+++ src/requirementslib/models/requirements.py
@@ -803,7 +803,7 @@ class Requirement(object):
             args["hashes"] = _pipfile.get("hashes", [pipfile.get("hash")])
         return cls(**args)
 
-    def as_line(self, sources=None):
+    def as_line(self, sources=None, include_hashes=True):
         """Format this requirement as a line in requirements.txt.
 
         If `sources` provided, it should be an sequence of mappings, containing
@@ -812,20 +812,22 @@ class Requirement(object):
         If `sources` is omitted or falsy, no index information will be included
         in the requirement line.
         """
-        line = "{0}{1}{2}{3}{4}".format(
+        parts = [
             self.req.line_part,
             self.extras_as_pip if not self.is_vcs else "",
             self.specifiers if self.specifiers else "",
             self.markers_as_pip,
-            self.hashes_as_pip,
-        )
+        ]
+        if include_hashes:
+            parts.append(self.hashes_as_pip)
         if sources and not (self.requirement.local_file or self.vcs):
             from ..utils import prepare_pip_source_args
 
             if self.index:
                 sources = [s for s in sources if s.get("name") == self.index]
             index_string = " ".join(prepare_pip_source_args(sources))
-            line = "{0} {1}".format(line, index_string)
+            parts.extend([" ", index_string])
+        line = "".join(parts)
         return line
 
     def get_markers(self):
