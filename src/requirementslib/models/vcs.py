71b521ac268c4ed33c4c9579ed526f3b33500166
Dan Ryan <dan@danryan.co>
2018-09-03 21:18:24 -0400

    Update vcs checkouts to avoid changing directories
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/requirementslib/models/vcs.py
+++ src/requirementslib/models/vcs.py
@@ -33,16 +33,12 @@ class VCSRepository(object):
     def obtain(self):
         if not os.path.exists(self.checkout_directory):
             self.repo_instance.obtain(self.checkout_directory)
-        sha = None
-        with vistir.contextmanagers.cd(self.checkout_directory):
-            if self.ref:
-                self.checkout_ref(self.ref)
-                sha = self.get_commit_hash(self.ref)
-            else:
-                if not self.commit_sha:
-                    sha = self.get_commit_hash()
-        if sha:
-            self.commit_hash = sha
+        if self.ref:
+            self.checkout_ref(self.ref)
+            self.commit_sha = self.get_commit_hash(self.ref)
+        else:
+            if not self.commit_sha:
+                self.commit_sha = self.get_commit_hash()
 
     def checkout_ref(self, ref):
         if not self.repo_instance.is_commit_id_equal(
@@ -63,10 +59,8 @@ class VCSRepository(object):
 
     def get_commit_hash(self, ref=None):
         sha = None
-        with vistir.contextmanagers.cd(self.checkout_directory):
-            if ref:
-                target_ref = self.repo_instance.make_rev_options(ref)
-                sha = self.repo_instance.get_revision_sha(self.checkout_directory, target_ref.arg_rev)
-                # return self.repo_instance.get_revision(self.checkout_directory)
-            sha = self.repo_instance.get_revision(self.checkout_directory)
-        return sha
+        if ref:
+            target_ref = self.repo_instance.make_rev_options(ref)
+            return self.repo_instance.get_revision_sha(self.checkout_directory, target_ref.arg_rev)
+            # return self.repo_instance.get_revision(self.checkout_directory)
+        return self.repo_instance.get_revision(self.checkout_directory)
