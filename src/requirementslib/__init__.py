20db93fcca5cfed019851e95e8d90fb49debbb69
Dan Ryan <dan@danryan.co>
2018-11-11 14:11:57 -0500

    Add exceptions and pyproject support
    - Fix typos in `InstallRequirement` generation
    - General cleanup
    - Improved error handling during lockfile load
    - Add ability to load lockfile from pipfile
    
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/requirementslib/__init__.py
+++ src/requirementslib/__init__.py
@@ -1,10 +1,13 @@
 # -*- coding=utf-8 -*-
 __version__ = '1.2.6.dev0'
 
+import warnings
+
 import logging
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
+warnings.filterwarnings("ignore", category=ResourceWarning)
 
 from .models.requirements import Requirement
 from .models.lockfile import Lockfile
