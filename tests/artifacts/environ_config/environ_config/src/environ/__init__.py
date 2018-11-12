bd5ee52eecb50a1fe4989980558a5d84d4bf9575
Dan Ryan <dan@danryan.co>
2018-11-11 23:12:16 -0500

    Add test coverage for local and remote artifacts
    Signed-off-by: Dan Ryan <dan@danryan.co>

---
+++ tests/artifacts/environ_config/environ_config/src/environ/__init__.py
@@ -0,0 +1,27 @@
+from . import secrets
+from ._environ_config import bool_var, config, group, to_config, var
+from .exceptions import MissingEnvValueError
+
+
+__version__ = "18.3.0.dev0"
+
+__title__ = "environ_config"
+__description__ = "Boilerplate-free configuration with env variables."
+__uri__ = "https://github.com/hynek/environ_config"
+
+__author__ = "Hynek Schlawack"
+__email__ = "hs@ox.cx"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2017 {0}".format(__author__)
+
+
+__all__ = [
+    "MissingEnvValueError",
+    "bool_var",
+    "config",
+    "group",
+    "secrets",
+    "to_config",
+    "var",
+]
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,12 @@
 from __future__ import absolute_import, print_function
 
 import os
+import shutil
 
 import pytest
 import vistir
+
+import requirementslib.utils
 
 
 def check_for_mercurial():
@@ -43,3 +46,38 @@ def pip_src_dir(request, pathlib_tmpdir):
 
     request.addfinalizer(finalize)
     return request
+
+
+@pytest.fixture(scope="session")
+def artifact_dir():
+    return vistir.compat.Path(__file__).absolute().parent.joinpath("artifacts")
+
+
+@pytest.fixture
+def test_artifact(artifact_dir, pathlib_tmpdir, request):
+    name = request.param["name"]
+    as_artifact = request.param.get("as_artifact", False)
+    target = artifact_dir.joinpath(name)
+    if target.exists():
+        if as_artifact:
+            installable = next(iter(sorted((
+                path for path in target.iterdir()
+                if requirementslib.utils.is_installable_file(path.as_posix())
+                and not path.is_dir()
+            ), reverse=True)), None)
+            if installable:
+                target_path = pathlib_tmpdir.joinpath(installable.name)
+                shutil.copy(installable.as_posix(), target_path.as_posix())
+                yield target_path
+                target_path.unlink()
+        else:
+            installable = next(iter(sorted((
+                path for path in target.iterdir()
+                if requirementslib.utils.is_installable_file(path.as_posix())
+                and path.is_dir()
+            ), reverse=True)), None)
+            if installable:
+                target_path = pathlib_tmpdir.joinpath(installable.name)
+                shutil.copytree(installable.as_posix(), target_path.as_posix())
+                yield target_path
+
