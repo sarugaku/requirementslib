7036e719ea188428b103dffdac4c6d4b2b393704
Dan Ryan <dan@danryan.co>
2018-11-19 17:55:21 -0500

    Ensure that we capture named extras sections
    - Add test to prevent regression
    - Fixes #116
    
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/requirementslib/models/setup_info.py
+++ src/requirementslib/models/setup_info.py
@@ -131,25 +131,41 @@ def get_metadata(path, pkg_name=None):
             None,
         )
         if dist:
-            requires = dist.requires()
-            dep_map = dist._build_dep_map()
+            try:
+                requires = dist.requires()
+            except exception:
+                requires = []
+            try:
+                dep_map = dist._build_dep_map()
+            except Exception:
+                dep_map = {}
             deps = []
+            extras = {}
             for k in dep_map.keys():
                 if k is None:
                     deps.extend(dep_map.get(k))
                     continue
                 else:
+                    extra = None
                     _deps = dep_map.get(k)
-                    k = k.replace(":", "; ")
+                    if k.startswith(":python_version"):
+                        marker = k.replace(":", "; ")
+                    else:
+                        marker = ""
+                        extra = "{0}".format(k)
                     _deps = [
-                        pkg_resources.Requirement.parse("{0}{1}".format(str(req), k))
+                        pkg_resources.Requirement.parse("{0}{1}".format(str(req), marker))
                         for req in _deps
                     ]
-                    deps.extend(_deps)
+                    if extra:
+                        extras[extra] = _deps
+                    else:
+                        deps.extend(_deps)
             return {
                 "name": dist.project_name,
                 "version": dist.version,
                 "requires": requires,
+                "extras": extras
             }
 
 
@@ -158,7 +174,6 @@ class SetupInfo(object):
     name = attr.ib(type=str, default=None)
     base_dir = attr.ib(type=Path, default=None)
     version = attr.ib(type=packaging.version.Version, default=None)
-    extras = attr.ib(type=list, default=attr.Factory(list))
     requires = attr.ib(type=dict, default=attr.Factory(dict))
     build_requires = attr.ib(type=list, default=attr.Factory(list))
     build_backend = attr.ib(type=list, default=attr.Factory(list))
@@ -223,6 +238,8 @@ class SetupInfo(object):
         if self.setup_py is not None and self.setup_py.exists():
             target_cwd = self.setup_py.parent.as_posix()
             with cd(target_cwd), _suppress_distutils_logs():
+                # This is for you, Hynek
+                # see https://github.com/hynek/environ_config/blob/69b1c8a/setup.py
                 script_name = self.setup_py.as_posix()
                 args = ["egg_info"]
                 g = {"__file__": script_name, "__name__": "__main__"}
@@ -231,8 +248,6 @@ class SetupInfo(object):
                     save_argv = sys.argv
                 else:
                     save_argv = sys.argv.copy()
-                # This is for you, Hynek
-                # see https://github.com/hynek/environ_config/blob/69b1c8a/setup.py
                 try:
                     global _setup_distribution, _setup_stop_after
                     _setup_stop_after = "run"
@@ -288,6 +303,15 @@ class SetupInfo(object):
                 self.requires.update(
                     {req.key: req for req in metadata.get("requires", {})}
                 )
+                if getattr(self.ireq, "extras", None):
+                    for extra in self.ireq.extras:
+                        self.requires.update(
+                            {
+                                req.key: req for req
+                                in metadata.get("extras", {}).get(extra)
+                                if req is not None
+                            }
+                        )
 
     def run_pyproject(self):
         if self.pyproject and self.pyproject.exists():
--- tests/unit/test_setup_info.py
+++ tests/unit/test_setup_info.py
@@ -3,6 +3,7 @@
 import pytest
 import sys
 import os
+import vistir
 
 from requirementslib.models.requirements import Requirement
 
@@ -42,3 +43,52 @@ def test_no_duplicate_egg_info():
     egg_info_name = "{}.egg-info".format(r.name.replace("-", "_"))
     assert os.path.isdir(os.path.join(base_dir, "src", egg_info_name))
     assert not os.path.isdir(os.path.join(base_dir, egg_info_name))
+
+
+def test_extras(pathlib_tmpdir):
+    """Test named extras as a dependency"""
+    setup_dir = pathlib_tmpdir.joinpath("test_package")
+    setup_dir.mkdir()
+    setup_py = setup_dir.joinpath("setup.py")
+    setup_py.write_text(u"""
+import os
+from setuptools import setup, find_packages
+
+thisdir = os.path.abspath(os.path.dirname(__file__))
+version = "1.0.0"
+
+testing_extras = [
+    'coverage',
+    'flaky',
+]
+
+setup(
+    name='test_package',
+    version=version,
+    description="The Backend HTTP Server",
+    long_description="This is a package",
+    install_requires=[
+        'six',
+    ],
+    tests_require=testing_extras,
+    extras_require={
+        'testing': testing_extras,
+    },
+    package_dir={"": "src"},
+    packages=['test_package'],
+    include_package_data=True,
+    zip_safe=False,
+) """.strip())
+    src_dir = setup_dir.joinpath("src")
+    src_dir.mkdir()
+    pkg_dir = src_dir.joinpath("test_package")
+    pkg_dir.mkdir()
+    pkg_dir.joinpath("__init__.py").write_text(u"")
+    pipfile_entry = {"path": "./{0}".format(setup_dir.name), "extras": ["testing"], "editable": True}
+
+    with vistir.contextmanagers.cd(pathlib_tmpdir.as_posix()):
+        r = Requirement.from_pipfile("test-package", pipfile_entry)
+        assert r.name == "test-package"
+        r.run_requires()
+        setup_dict = r.req.setup_info.as_dict()
+        assert sorted(list(setup_dict.get("requires").keys())) == ["coverage", "flaky", "six"]
