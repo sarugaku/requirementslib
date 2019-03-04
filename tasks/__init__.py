41d1e0a2725f2008299ba40477169e7d1dd53877
Dan Ryan <dan@danryan.co>
2019-03-04 12:46:08 -0500

    Fix release task to use double quotes and reformat
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,30 +1,26 @@
 # -*- coding=utf-8 -*-
 # Copyied from pip's vendoring process
 # see https://github.com/pypa/pip/blob/95bcf8c5f6394298035a7332c441868f3b0169f4/tasks/__init__.py
-import invoke
-
-from . import vendoring
-from . import news
-
 import enum
 import pathlib
+import re
 import shutil
 import subprocess
-import parver
-import re
 import sys
 from pathlib import Path
 
-from towncrier._builder import (
-    find_fragments, render_fragments, split_fragments,
-)
+import invoke
+import parver
+from towncrier._builder import find_fragments, render_fragments, split_fragments
 from towncrier._settings import load_config
+
+from . import news, vendoring
 
 ROOT = pathlib.Path(__file__).resolve().parent.parent
 
-PACKAGE_NAME = 'requirementslib'
+PACKAGE_NAME = "requirementslib"
 
-INIT_PY = ROOT.joinpath('src', PACKAGE_NAME, '__init__.py')
+INIT_PY = ROOT.joinpath("src", PACKAGE_NAME, "__init__.py")
 
 
 class LogLevel(enum.Enum):
@@ -36,7 +32,7 @@ class LogLevel(enum.Enum):
 
 
 def _get_git_root(ctx):
-    return Path(ctx.run('git rev-parse --show-toplevel', hide=True).stdout.strip())
+    return Path(ctx.run("git rev-parse --show-toplevel", hide=True).stdout.strip())
 
 
 def find_version():
@@ -60,21 +56,21 @@ def typecheck(ctx):
 def clean(ctx):
     """Clean previously built package artifacts.
     """
-    ctx.run(f'python setup.py clean')
-    dist = ROOT.joinpath('dist')
-    print(f'[clean] Removing {dist}')
+    ctx.run(f"python setup.py clean")
+    dist = ROOT.joinpath("dist")
+    print(f"[clean] Removing {dist}")
     if dist.exists():
         shutil.rmtree(str(dist))
 
 
 def _read_version():
-    out = subprocess.check_output(['git', 'tag'], encoding='ascii')
+    out = subprocess.check_output(["git", "tag"], encoding="ascii")
     versions = [line.strip() for line in out.splitlines() if line]
-    _unparsed = [v for v in versions if v.startswith('v')]
+    _unparsed = [v for v in versions if v.startswith("v")]
     try:
         version = max(parver.Version.parse(v.lstrip("v")).normalize() for v in versions)
     except ValueError:
-        version = parver.Version.parse('0.0.0')
+        version = parver.Version.parse("0.0.0")
     return version
 
 
@@ -82,54 +78,54 @@ def _write_version(v):
     lines = []
     with INIT_PY.open() as f:
         for line in f:
-            if line.startswith('__version__ = '):
-                line = f'__version__ = {repr(str(v))}\n'
+            if line.startswith("__version__ = "):
+                line = f"__version__ = {repr(str(v))}\n".replace("'", '"')
             lines.append(line)
-    with INIT_PY.open('w', newline='\n') as f:
-        f.write(''.join(lines))
+    with INIT_PY.open("w", newline="\n") as f:
+        f.write("".join(lines))
 
 
 def _render_log():
     """Totally tap into Towncrier internals to get an in-memory result.
     """
     config = load_config(ROOT)
-    definitions = config['types']
+    definitions = config["types"]
     fragments, fragment_filenames = find_fragments(
-        pathlib.Path(config['directory']).absolute(),
-        config['sections'],
+        pathlib.Path(config["directory"]).absolute(),
+        config["sections"],
         None,
         definitions,
     )
     rendered = render_fragments(
-        pathlib.Path(config['template']).read_text(encoding='utf-8'),
-        config['issue_format'],
+        pathlib.Path(config["template"]).read_text(encoding="utf-8"),
+        config["issue_format"],
         split_fragments(fragments, definitions),
         definitions,
-        config['underlines'][1:],
+        config["underlines"][1:],
         False,  # Don't add newlines to wrapped text.
     )
     return rendered
 
 
-REL_TYPES = ('major', 'minor', 'patch', 'post')
+REL_TYPES = ("major", "minor", "patch", "post")
 
 
 def _bump_release(version, type_):
     if type_ not in REL_TYPES:
-        raise ValueError(f'{type_} not in {REL_TYPES}')
+        raise ValueError(f"{type_} not in {REL_TYPES}")
     index = REL_TYPES.index(type_)
     next_version = version.base_version().bump_release(index=index)
-    print(f'[bump] {version} -> {next_version}')
+    print(f"[bump] {version} -> {next_version}")
     return next_version
 
 
 def _prebump(version, prebump):
     next_version = version.bump_release(index=prebump).bump_dev()
-    print(f'[bump] {version} -> {next_version}')
+    print(f"[bump] {version} -> {next_version}")
     return next_version
 
 
-PREBUMP = 'patch'
+PREBUMP = "patch"
 
 
 @invoke.task(pre=[clean])
@@ -137,7 +133,7 @@ def release(ctx, type_, repo, prebump=PREBUMP):
     """Make a new release.
     """
     if prebump not in REL_TYPES:
-        raise ValueError(f'{type_} not in {REL_TYPES}')
+        raise ValueError(f"{type_} not in {REL_TYPES}")
     prebump = REL_TYPES.index(prebump)
 
     version = _read_version()
@@ -147,26 +143,26 @@ def release(ctx, type_, repo, prebump=PREBUMP):
     # Needs to happen before Towncrier deletes fragment files.
     tag_content = _render_log()
 
-    ctx.run('towncrier')
+    ctx.run("towncrier")
 
     ctx.run(f'git commit -am "Release v{version}"')
 
     tag_content = tag_content.replace('"', '\\"')
     ctx.run(f'git tag -a v{version} -m "Version v{version}\n\n{tag_content}"')
 
-    ctx.run(f'python setup.py sdist bdist_wheel')
+    ctx.run(f"python setup.py sdist bdist_wheel")
 
     dist_pattern = f'{PACKAGE_NAME.replace("-", "[-_]")}-*'
-    artifacts = list(ROOT.joinpath('dist').glob(dist_pattern))
-    filename_display = '\n'.join(f'  {a}' for a in artifacts)
-    print(f'[release] Will upload:\n{filename_display}')
+    artifacts = list(ROOT.joinpath("dist").glob(dist_pattern))
+    filename_display = "\n".join(f"  {a}" for a in artifacts)
+    print(f"[release] Will upload:\n{filename_display}")
     try:
-        input('[release] Release ready. ENTER to upload, CTRL-C to abort: ')
+        input("[release] Release ready. ENTER to upload, CTRL-C to abort: ")
     except KeyboardInterrupt:
-        print('\nAborted!')
+        print("\nAborted!")
         return
 
-    arg_display = ' '.join(f'"{n}"' for n in artifacts)
+    arg_display = " ".join(f'"{n}"' for n in artifacts)
     ctx.run(f'twine upload --repository="{repo}" {arg_display}')
 
     version = _prebump(version, prebump)
@@ -179,9 +175,9 @@ def release(ctx, type_, repo, prebump=PREBUMP):
 def build_docs(ctx):
     _current_version = find_version()
     minor = _current_version.split(".")[:2]
-    docs_folder = (_get_git_root(ctx) / 'docs').as_posix()
-    if not docs_folder.endswith('/'):
-        docs_folder = '{0}/'.format(docs_folder)
+    docs_folder = (_get_git_root(ctx) / "docs").as_posix()
+    if not docs_folder.endswith("/"):
+        docs_folder = "{0}/".format(docs_folder)
     args = ["--ext-autodoc", "--ext-viewcode", "-o", docs_folder]
     args.extend(["-A", "'Dan Ryan <dan@danryan.co>'"])
     args.extend(["-R", _current_version])
@@ -195,7 +191,12 @@ def build_docs(ctx):
 def clean_mdchangelog(ctx):
     changelog = ROOT / "CHANGELOG.md"
     content = changelog.read_text()
-    content = re.sub(r"([^\n]+)\n?\s+\[[\\]+(#\d+)\]\(https://github\.com/sarugaku/[\w\-]+/issues/\d+\)", r"\1 \2", content, flags=re.MULTILINE)
+    content = re.sub(
+        r"([^\n]+)\n?\s+\[[\\]+(#\d+)\]\(https://github\.com/sarugaku/[\w\-]+/issues/\d+\)",
+        r"\1 \2",
+        content,
+        flags=re.MULTILINE,
+    )
     changelog.write_text(content)
 
 
@@ -233,4 +234,6 @@ def profile(ctx, filepath, calltree=False):
             ctx.run(f"vprof -c cmhp {filepath!s}")
 
 
-ns = invoke.Collection(build_docs, vendoring, news, release, clean_mdchangelog, profile, typecheck)
+ns = invoke.Collection(
+    build_docs, vendoring, news, release, clean_mdchangelog, profile, typecheck
+)
