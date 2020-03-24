98b8e45a00ae327d6efb97785ffa03d779c715b1
Dan Ryan <dan.ryan@canonical.com>
2020-03-24 18:23:59 -0400

    Fix newsfile generation script
    Signed-off-by: Dan Ryan <dan.ryan@canonical.com>

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -104,11 +104,6 @@ def _render_log():
         None,
         definitions,
     )
-    project_options = {
-        "name": config["package"],
-        "version": _read_text_version(),
-        "date": datetime.date.today().isoformat(),
-    }
     rendered = render_fragments(
         pathlib.Path(config["template"]).read_text(encoding="utf-8"),
         config["issue_format"],
@@ -116,7 +111,6 @@ def _render_log():
         definitions,
         config["underlines"][1:],
         False,  # Don't add newlines to wrapped text.
-        project_options,
     )
     return rendered
 
