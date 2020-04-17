59b4ce06830cec7a2c7b0524f16d1ef8f21050d4
Michael Marino <michael.marino@tado.com>
2020-04-17 16:00:58 +0200

    Add test for env variable usage in setup

---
+++ tests/fixtures/setup_py/package_with_dependence_on_env_vars/setup.py
@@ -0,0 +1,15 @@
+import os
+
+from setuptools import find_packages, setup
+
+extras_require = {
+    "docs": ["sphinx", "sphinx-argparse"],
+}
+setup(
+    name="a",
+    version="a",
+    description="a",
+    packages=find_packages(),
+    extras_require=extras_require,
+    install_requires=extras_require["docs"] if "READTHEDOCS" in os.environ else [],
+)
