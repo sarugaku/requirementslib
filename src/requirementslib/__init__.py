ccdcd21b690974ce23dc11564b04f7ddbecc5fa3
Github Action <action@github.com>
2020-04-22 06:29:01 +0000

    pre-bump

--- src/requirementslib/__init__.py
+++ src/requirementslib/__init__.py
@@ -10,7 +10,7 @@ from .models.lockfile import Lockfile
 from .models.pipfile import Pipfile
 from .models.requirements import Requirement
 
-__version__ = "1.5.6"
+__version__ = "1.5.7.dev0"
 
 
 logger = logging.getLogger(__name__)
