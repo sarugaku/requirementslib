2841a2b052909c246881d5063f6fc7bfea6ccae9
Frost Ming <mianghong@gmail.com>
2020-10-29 16:01:47 +0800

    add some debug steps

---
+++ pip_supported.py
@@ -0,0 +1,9 @@
+from pip._internal.cli.cmdoptions import make_target_python
+from pip._internal.commands.install import InstallCommand
+
+install_cmd = InstallCommand("install", "test")
+options, _ = install_cmd.parse_args([])
+target_python = make_target_python(options)
+print("=========== SUPPORTED TAGS =============")
+print(*target_python.get_tags(), sep="\n")
+print("=========== END TAGS ===============")
