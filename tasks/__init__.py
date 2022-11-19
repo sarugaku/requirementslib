3ce26d3c8ad51f4f790eb51b20aa498ff887ff93
Oz Tiram <oz.tiram@gmail.com>
2022-11-19 14:53:38 +0100

    Bump version to 2.2.0

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -109,8 +109,8 @@ def _render_log():
         False,  # Don't add newlines to wrapped text.
         {
             "name": "requirementslib",
-            "version": "2.1.0",
-            "date": "2022-10-08",
+            "version": "2.2.0",
+            "date": "2022-11-19",
         },  # towncrier==19.9.0
     )
     return rendered
