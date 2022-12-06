59fe02bccdccc5869bfa8d40c1f043d056f86fe3
Oz Tiram <oz.tiram@gmail.com>
2022-12-06 10:41:07 +0100

    Release 2.2.1

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -109,8 +109,8 @@ def _render_log():
         False,  # Don't add newlines to wrapped text.
         {
             "name": "requirementslib",
-            "version": "2.2.0",
-            "date": "2022-11-19",
+            "version": "2.2.1",
+            "date": "2022-12-06",
         },  # towncrier==19.9.0
     )
     return rendered
